1a9bce4c2c9da3cdffe338281667dc325eb4666c
Eldor Ruziev <119814565+Eldor575636@users.noreply.github.com>
2026-06-24 14:37:32 -0700

    fix: remove audience from Auth0 config to fix login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,7 +16,6 @@ root.render(
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin + '/auth/callback',
-        ...(process.env.REACT_APP_AUTH0_AUDIENCE ? { audience: process.env.REACT_APP_AUTH0_AUDIENCE } : {}),
         scope: 'openid profile email',
       }}
     >
